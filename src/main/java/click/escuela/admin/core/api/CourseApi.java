fe565079e9db7b9c54ebd51953ca0f1e46dc048f
Evelin Aragon <eve_aragon@hotmail.com>
2021-04-23 22:05:05 -0300

    PRUEBA6

--- src/main/java/click/escuela/admin/core/api/CourseApi.java
+++ src/main/java/click/escuela/admin/core/api/CourseApi.java
@@ -1,12 +1,15 @@
 package click.escuela.admin.core.api;
-
+package click.escuela.admin.core.api;
+package click.escuela.admin.core.api;
+/*kfdhkhfdk*/
 import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
-
+/*kfdhkhfdk*/
 @Getter
 @Setter
 public class CourseApi {
 	private String name;
 	private List<String> students;
 }
+/*kfdhkhfdk*//*kfdhkhfdk*//*kfdhkhfdk*//*kfdhkhfdk*//*kfdhkhfdk*/
